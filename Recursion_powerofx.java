23a4f0b521c12dfdbf2cafbcd607fb7db2277103
kshitiz sharma <107552525+KshitizKaushik2@users.noreply.github.com>
2023-07-15 01:52:52 +0530

    This is optimized solution

--- Recursion_powerofx.java
+++ Recursion_powerofx.java
@@ -1,13 +1,26 @@
 public class Recursion_powerofx {
+    public static int power(int x, int n) {
 
-    public static int power(int x, int n){
-        if(n == 0){
+        // this is optimized sollution
+        if (n == 0) {
             return 1;
         }
-        // int xn1 = power(x,n-1);
-        // int xfinal = x * xn1;
-        return x * power(x, n-1);
+        int halfPower = power(x, n / 2) * power(x, n / 2);
+
+        if (n % 2 != 0) {
+            halfPower = x * halfPower;
+        }
+        return halfPower;
+
+        // this solution is not optimized
+        // if(n == 0){
+        // return 1;
+        // }
+        // // int xn1 = power(x,n-1);
+        // // int xfinal = x * xn1;
+        // return x * power(x, n-1);
     }
+
     public static void main(String[] args) {
         System.out.println(power(2, 5));
     }
